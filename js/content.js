d9e2fdc501495b9bcd8cc0546a2c3abc1a75a841
octop162 <octop162@icloud.com>
2022-10-31 19:34:48 +0900

    リファクタリング

--- js/content.js
+++ js/content.js
@@ -4,10 +4,13 @@
  */
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "GET_MEMBERS") {
-    let members = document.querySelectorAll(
-      "div[role='list'] > div[role='listitem'] > div > div > div > span:first-child"
-    );
-    sendResponse(Object.values(members).map((member) => member.textContent));
-    return true;
+    switch (message.action) {
+      case "GET_MEMBERS":
+        let members = document.querySelectorAll(
+          "div[role='list'] > div[role='listitem'] > div > div > div > span:first-child"
+        );
+        sendResponse(Object.values(members).map((member) => member.textContent));
+        return true;
+    }
   }
 });
